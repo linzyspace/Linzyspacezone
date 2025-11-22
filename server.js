4da720fffc938e4dcd4229ec0630be19f8c96a21
linzyspace <linzyspace@gmail.com>
2025-11-21 18:43:12 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,28 @@ import { fileURLToPath } from "url";
 
 const app = express();
 
-// Allow iframe embedding (important for Blogger)
+// Allow embedding inside Blogger iframe
 app.use((req, res, next) => {
   res.setHeader("X-Frame-Options", "ALLOWALL");
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
-// Fix dirname for ESM
+// ESM fix
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve public folder
+// Serve /public
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Handle iframe query: /?ask=question
+app.get("/ask", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
-  console.log("Server running on port " + port)
-);
+app.listen(port, () => console.log("Server running on " + port));
