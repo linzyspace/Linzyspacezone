84129bc92a7b8883a73ae8690bbb8c569af59386
linzyspace <linzyspace@gmail.com>
2025-11-21 17:01:20 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+import express from "express";
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/public/index.html");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server running on " + port));
 
